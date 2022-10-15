197385abd9cc3446cb8aedadc6721ce36776ca3c
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-15 10:10:49 +0600

    css framework and router setup

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBodXsHb4xljhfBcKUuavCAHoIZ-rSyHfo",
+  authDomain: "practice-app-4616a.firebaseapp.com",
+  projectId: "practice-app-4616a",
+  storageBucket: "practice-app-4616a.appspot.com",
+  messagingSenderId: "979950810584",
+  appId: "1:979950810584:web:96ef8283c37ef5bc206ead"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
