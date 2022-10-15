a2dd0957db44510ca3d9e6aa0765e988cc6df652
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-15 11:02:42 +0600

    header footer and signup page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-
+import Signin from './Components/Signin';
+import Signup from './Components/Signup';
+import Error from './Components/Error'
+import Main from './layouts/Main';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: '/',
+        element: <Signin></Signin>
+      },
+      {
+        path: '/signin',
+        element: <Signin></Signin>
+      },
+      {
+        path: '/signup',
+        element: <Signup></Signup>
+      }
+    ]
+  }
+])
 function App() {
   return (
     <div className="App">
-      <h1>This is from tailwind css</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
